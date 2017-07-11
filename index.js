3d304a8100a8e7ec6e7d1dbd038bfc831fa7c872
System <none>
2017-07-11 20:47:32 +0000

    Checkpoint

--- index.js
+++ index.js
@@ -2,3 +2,16 @@ const express = require("express");
 
 var app = express();
 
+app.get("/", function (req, res) {
+  var params = req.params;
+  if (query) {
+    if () {
+        
+    } else {
+      res.send();    
+    }
+  }
+  res.send( JSON.stringify(query) );
+});
+
+app.listen();
