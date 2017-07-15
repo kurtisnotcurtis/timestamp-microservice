6058ae8b10219e6d423356d67aa545fa492fbe91
System <none>
2017-07-15 19:20:01 +0000

    Checkpoint

--- index.js
+++ index.js
@@ -5,31 +5,37 @@ const path = require("path");
 const port = process.env.PORT || 3000;
 var app = express();
 
-app.get("/", function (req, res) {
-
+app.get("/", function (req, res) { // Serve index page
+  var fileName = path.join(__dirname, "views", "index.html");
+  res.sendFile(fileName, function (err) {
+    if (err) {
+      console.log(err);
+      res.status(err.status).end();
+    } else {
+      console.log("View sent:", fileName);
+    }
+  })
 });
   
-app.get("/:datestr", function (req, res) {
+app.get("/:datestr", function (req, res) { // Handle passed param string
   var params = req.params;
-  if (params) {
     
-    var response;
-    
-    if ( /%20/.test(params) ) {
-      // Request is formatted in natural language
-      response = {
-        unix: new Date(),
-        natural: req.params
-      };
-    } else {
-      // Request contains a Unix timestamp
-      response = {
-        unix: req.params,
-        natural: new Date()
-      };
-    }
-    res.send( JSON.stringify(params) );
+  var date;
+
+  if ( /%20/.test(params) ) {
+    // Request is formatted in natural language
+    response = {
+      unix: new Date(),
+      natural: req.params
+    };
+  } else {
+    // Request contains a Unix timestamp
+    response = {
+      unix: req.params,
+      natural: new Date()
+    };
   }
+  res.send( JSON.stringify(params) );
 });
   
 
