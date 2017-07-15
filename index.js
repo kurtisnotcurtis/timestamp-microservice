9140cb5a478b5aead0f5f9b3904243ff319c7251
System <none>
2017-07-15 18:58:01 +0000

    Checkpoint

--- index.js
+++ index.js
@@ -4,14 +4,25 @@ var app = express();
 
 app.get("/", function (req, res) {
   var params = req.params;
-  if (query) {
-    if () {
-        
+  if (params) {
+    
+    var response;
+    
+    if ( /%20/.test(params) ) {
+      // Request is formatted in natural language
+      response = {
+        unix: ?,
+        natural: req.params
+      };
     } else {
-      res.send();    
+      // Request contains a Unix timestamp
+      response = {
+        unix: req.params,
+        natural: ?
+      };
     }
+    res.send( JSON.stringify(params) );
   }
-  res.send( JSON.stringify(query) );
 });
 
 app.listen(process.env.PORT);
